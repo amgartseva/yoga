542aebf4b0ccd63bb037be65cbe89602ff41917f
Anastasia Gartseva <amgartseva@gmail.com>
2019-09-25 17:46:01 +0300

    Слайдер 4 экрана

--- source/js/main.js
+++ source/js/main.js
@@ -12,17 +12,35 @@ var reviewsActivePage = reviewsSlider.querySelector('.review-slider__active-page
 var reviewsLastPage = reviewsSlider.querySelector('.review-slider__last-page');
 var reviewsSlides = Array.from(reviewsSlider.querySelectorAll('.review-slider__item'));
 
+var team = document.querySelector('.team');
+var teamSlider = team.querySelector('.teachers-list');
+var teamSliderLeftBtn = team.querySelector('.controls__left-btn');
+var teamSliderRightBtn = team.querySelector('.controls__right-btn');
+var teamActivePage = team.querySelector('.controls__active-page');
+var teamLastPage = team.querySelector('.controls__last-page');
+var teamSlides = Array.from(teamSlider.querySelectorAll('.teachers-list__item'));
+
 if (screenWidth <= MOBILE) {
   reviewsSlides[0].classList.add('review-slider__item--active');
+  teamSlides[0].classList.add('teachers-list__item--active');
   reviewsSliderRightBtn.addEventListener('click', onSliderRightBtnMobile);
   reviewsSliderLeftBtn.addEventListener('click', onSliderLeftBtnMobile);
+  teamSliderRightBtn.addEventListener('click', onTeamSliderRightBtnMobile);
+  teamSliderLeftBtn.addEventListener('click', onTeamSliderLeftBtnMobile);
 } else {
   reviewsSlides[0].classList.add('review-slider__item--active');
-  if (reviewsSlides.length >= 1) {
+  teamSlides[0].classList.add('teachers-list__item--active');
+  if (reviewsSlides.length >= 2) {
     reviewsSlides[1].classList.add('review-slider__item--active');
-    reviewsSliderRightBtn.addEventListener('click', onSliderRightBtn);
-    reviewsSliderLeftBtn.addEventListener('click', onSliderLeftBtn);
+    teamSlides[1].classList.add('teachers-list__item--active');
   }
+  if (reviewsSlides.length >= 3) {
+    teamSlides[2].classList.add('teachers-list__item--active');
+  }
+  reviewsSliderRightBtn.addEventListener('click', onSliderRightBtn);
+  reviewsSliderLeftBtn.addEventListener('click', onSliderLeftBtn);
+  teamSliderRightBtn.addEventListener('click', onTeamSliderRightBtn);
+  teamSliderLeftBtn.addEventListener('click', onTeamSliderLeftBtn);
 }
 
 function onSliderRightBtn () {
@@ -61,7 +79,7 @@ function onSliderLeftBtn () {
 function onSliderRightBtnMobile () {
   var curSlideIndex = reviewsSlides.indexOf(reviewsSlider.querySelector('.review-slider__item--active'));
   var nextSlideIndex = 0;
-  var maxIndex = reviewsSlides.length - 2;
+  var maxIndex = reviewsSlides.length - 1;
   var nextPage = 1;
 
   reviewsSlides[curSlideIndex].classList.remove('review-slider__item--active');
@@ -82,4 +100,67 @@ function onSliderLeftBtnMobile () {
     nextSlideIndex = curSlideIndex - 1;
   }
   reviewsSlides[nextSlideIndex].classList.add('review-slider__item--active');
+}
+
+function onTeamSliderRightBtn () {
+  var curSlideIndex = teamSlides.indexOf(teamSlider.querySelector('.teachers-list__item--active'));
+  var nextSlideIndex = 0;
+  var maxIndex = teamSlides.length - 3;
+  var nextPage = 1;
+
+  teamSlides[curSlideIndex].classList.remove('teachers-list__item--active');
+  teamSlides[curSlideIndex + 1].classList.remove('teachers-list__item--active');
+  teamSlides[curSlideIndex + 2].classList.remove('teachers-list__item--active');
+  if (curSlideIndex !== maxIndex) {
+    nextSlideIndex = curSlideIndex + 3;
+    nextPage = +reviewsActivePage.textContent + 1;
+  }
+  teamSlides[nextSlideIndex].classList.add('teachers-list__item--active');
+  teamSlides[nextSlideIndex + 1].classList.add('teachers-list__item--active');
+  teamSlides[nextSlideIndex + 2].classList.add('teachers-list__item--active');
+  teamActivePage.innerHTML = nextPage;
+}
+
+function onTeamSliderLeftBtn () {
+  var curSlideIndex = teamSlides.indexOf(teamSlider.querySelector('.teachers-list__item--active'));
+  var nextSlideIndex = teamSlides.length - 3;
+  var nextPage = +teamLastPage.textContent;
+
+  teamSlides[curSlideIndex].classList.remove('teachers-list__item--active');
+  teamSlides[curSlideIndex + 1].classList.remove('teachers-list__item--active');
+  teamSlides[curSlideIndex + 2].classList.remove('teachers-list__item--active');
+  if (curSlideIndex !== 0) {
+    nextSlideIndex = curSlideIndex - 3;
+    nextPage = +teamActivePage.textContent - 1;
+  }
+  teamSlides[nextSlideIndex].classList.add('teachers-list__item--active');
+  teamSlides[nextSlideIndex + 1].classList.add('teachers-list__item--active');
+  teamSlides[nextSlideIndex + 2].classList.add('teachers-list__item--active');
+  teamsActivePage.innerHTML = nextPage;
+}
+
+function onTeamSliderRightBtnMobile () {
+  var curSlideIndex = teamSlides.indexOf(teamSlider.querySelector('.teachers-list__item--active'));
+  var nextSlideIndex = 0;
+  var maxIndex = teamSlides.length - 1;
+  var nextPage = 1;
+
+  teamSlides[curSlideIndex].classList.remove('teachers-list__item--active');
+  if (curSlideIndex !== maxIndex) {
+    nextSlideIndex = curSlideIndex + 1;
+    nextPage = teamActivePage.textContent + 1;
+  }
+  teamSlides[nextSlideIndex].classList.add('teachers-list__item--active');
+  teamActivePage.innerHTML = nextPage;
+}
+
+function onTeamSliderLeftBtnMobile () {
+  var curSlideIndex = teamSlides.indexOf(teamSlider.querySelector('.teachers-list__item--active'));
+  var nextSlideIndex = teamSlides.length - 1;
+
+  teamSlides[curSlideIndex].classList.remove('teachers-list__item--active');
+  if (curSlideIndex !== 0) {
+    nextSlideIndex = curSlideIndex - 1;
+  }
+  teamSlides[nextSlideIndex].classList.add('teachers-list__item--active');
 }
