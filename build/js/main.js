0ce62f2def5358942d4bc585a73a0baff2f406df
Margarita <margaritk@inbox.ru>
2019-10-05 20:35:10 +0300

    Добавляет слайдер swiper.js в блок reviews и переключение тарифов по кнопкам

--- build/js/main.js
+++ build/js/main.js
@@ -15,54 +15,55 @@
 'use strict';
 
 (function () {
-  // var teamSwiper = document.querySelector('.team-container__slider');
-  // var emptyBlock = document.querySelector('.fill-js');
-  // var teamSlider = document.querySelector('.team-container__slider');
-  // var teamSlides = document.querySelectorAll('.teacher-card');
-  // var cloneDescription = teamSlides[0].querySelector('.teacher-card__description').cloneNode(true);
+  var morningPrice = document.querySelector('.options__price-morning');
+  var eveningPrice = document.querySelector('.options__price-evening');
+  var noLimitPrice = document.querySelector('.options__price-nolimit');
+  var buttonsBlock = document.querySelector('.length');
+  var buttons = document.querySelectorAll('.length__item');
 
-  // if (!teamSlider) {
-  //   return;
-  // }
+  buttonsBlock.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('length__item')) {
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('length__item--active');
+      }
 
-  // if (!teamSwiper) {
-  //   return;
-  // }
+      evt.target.classList.add('length__item--active');
+      morningPrice.textContent = evt.target.dataset.morning;
+      eveningPrice.textContent = evt.target.dataset.evening;
+      noLimitPrice.textContent = evt.target.dataset.nolimit;
+    }
+  });
+})();
 
-  // if (emptyBlock && cloneDescription) {
-  //   cloneDescription.className = 'team__description-wrapper';
-  //   emptyBlock.appendChild(cloneDescription);
-  // }
+'use strict';
 
-  // teamSlider.addEventListener('click', function (evt) {
-  //   if (evt.target.classList.contains('teacher-card__info-btn')) {
-  //     var description = evt.path[3].querySelector('.teacher-card__description');
-  //     cloneDescription = description.cloneNode(true);
-  //     cloneDescription.className = 'team__description-wrapper';
+(function () {
+  var reviewsSwiper = document.querySelector('.review-slider');
 
-  //     emptyBlock.innerHTML = '';
-  //     emptyBlock.appendChild(cloneDescription);
-  //   }
-  // });
+  if (!reviewsSwiper) {
+    return;
+  }
 
-  // (function () {
-  //   return new window.Swiper(teamSwiper, {
-  //     observeParents: true,
-  //     observer: true,
-  //     slidesPerView: 1,
-  //     slidesPerColumn: 3,
-  //     spaceBetween: 20,
-  //     breakpointsInverse: true,
-  //     pagination: {
-  //       el: '.swiper-pagination',
-  //       type: 'fraction',
-  //     },
-  //     navigation: {
-  //       nextEl: '.swiper-button-next',
-  //       prevEl: '.swiper-button-prev',
-  //     },
-  //   });
-  // })();
+  (function () {
+    return new window.Swiper(reviewsSwiper, {
+      observeParents: true,
+      observer: true,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 20,
+      breakpointsInverse: true,
+      loop: true,
+      autoHeight: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  })();
 })();
 
 'use strict';
--- source/js/modules/reviews-slider.js
+++ source/js/modules/reviews-slider.js
@@ -1,52 +1,30 @@
 'use strict';
 
 (function () {
-  // var teamSwiper = document.querySelector('.team-container__slider');
-  // var emptyBlock = document.querySelector('.fill-js');
-  // var teamSlider = document.querySelector('.team-container__slider');
-  // var teamSlides = document.querySelectorAll('.teacher-card');
-  // var cloneDescription = teamSlides[0].querySelector('.teacher-card__description').cloneNode(true);
+  var reviewsSwiper = document.querySelector('.review-slider');
 
-  // if (!teamSlider) {
-  //   return;
-  // }
+  if (!reviewsSwiper) {
+    return;
+  }
 
-  // if (!teamSwiper) {
-  //   return;
-  // }
-
-  // if (emptyBlock && cloneDescription) {
-  //   cloneDescription.className = 'team__description-wrapper';
-  //   emptyBlock.appendChild(cloneDescription);
-  // }
-
-  // teamSlider.addEventListener('click', function (evt) {
-  //   if (evt.target.classList.contains('teacher-card__info-btn')) {
-  //     var description = evt.path[3].querySelector('.teacher-card__description');
-  //     cloneDescription = description.cloneNode(true);
-  //     cloneDescription.className = 'team__description-wrapper';
-
-  //     emptyBlock.innerHTML = '';
-  //     emptyBlock.appendChild(cloneDescription);
-  //   }
-  // });
-
-  // (function () {
-  //   return new window.Swiper(teamSwiper, {
-  //     observeParents: true,
-  //     observer: true,
-  //     slidesPerView: 1,
-  //     slidesPerColumn: 3,
-  //     spaceBetween: 20,
-  //     breakpointsInverse: true,
-  //     pagination: {
-  //       el: '.swiper-pagination',
-  //       type: 'fraction',
-  //     },
-  //     navigation: {
-  //       nextEl: '.swiper-button-next',
-  //       prevEl: '.swiper-button-prev',
-  //     },
-  //   });
-  // })();
+  (function () {
+    return new window.Swiper(reviewsSwiper, {
+      observeParents: true,
+      observer: true,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 20,
+      breakpointsInverse: true,
+      loop: true,
+      autoHeight: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  })();
 })();
