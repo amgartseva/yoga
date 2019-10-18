ecaf390571a4532aa0733a3913b7a2645e8edad1
Eugenia <janevega25@gmail.com>
2019-10-18 18:49:02 +0300

    Исправляет слайдер с отзывами

--- source/js/modules/reviews-slider.js
+++ source/js/modules/reviews-slider.js
@@ -6,13 +6,13 @@
     return;
   }
 
-  /*if (window.matchMedia('(min-width: 1024px)').matches) {
     (function () {
       swiper = new window.Swiper(reviewsSwiper, {
         observeParents: true,
         observer: true,
-        slidesPerView: 2,
-        slidesPerGroup: 2,
+        slidesPerView: generateSlidesPerView(),
+        slidesPerGroup: generateSlidesPerGroup (),
+        slidesPerColumn: generateSlidesPerColumn(),
         spaceBetween: 20,
         breakpointsInverse: true,
 
@@ -27,45 +27,38 @@
         },
       });
     })();
-  } else
-    (function () {
-      return new window.Swiper(reviewsSwiper, {
-        observeParents: true,
-        observer: true,
-        slidesPerView: 1,
-        slidesPerColumn: 2,
-        spaceBetween: 20,
-        breakpointsInverse: true,
 
-        loop: true,
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'fraction',
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-    })();*/
+    function generateSlidesPerColumn () {
+      var number
+      if (window.matchMedia('(max-width: 1023px)').matches && window.matchMedia('(min-width: 767px)').matches) {
+        number = 2;
+      } else {
+        number = 1;
+      }
 
-    (function () {
-      return new window.Swiper(reviewsSwiper, {
-        observeParents: true,
-        observer: true,
-        slidesPerView: 1,
-        spaceBetween: 20,
-        breakpointsInverse: true,
+      return number
+    }
 
-        loop: true,
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'fraction',
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-    })();
+    function generateSlidesPerView () {
+      var number
+      if (window.matchMedia('(min-width: 1024px)').matches) {
+        number = 2;
+      } else {
+        number = 1;
+      }
+
+      return number
+    }
+
+    function generateSlidesPerGroup () {
+      var number
+      if (window.matchMedia('(min-width: 1024px)').matches) {
+        number = 2;
+      } else {
+        number = 1;
+      }
+
+      return number
+    }
+
 })();
